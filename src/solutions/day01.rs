390bf97d83adbb86e084ce38eb439bcd8d8a5f29
Floris Douw <Floris.Douw@ogd.nl>
2022-12-01 11:20:45 +0100

    Add solution to day 1

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,6 +1,12 @@
-pub fn solve(_input: String, _verbose: bool) -> (String, String) {
-    (
-        String::from("<not yet implemented>"),
-        String::from("<not yet implemented>"),
-    )
+pub fn solve(input: String, _verbose: bool) -> (String, String) {
+    let mut calorie_counts: Vec<u32> = input
+        .split("\n\n")
+        .map(|s| s.lines().map(|l| l.parse::<u32>().unwrap()).sum())
+        .collect();
+    calorie_counts.sort_by(|a, b| a.cmp(b));
+
+    let part1 = calorie_counts.iter().max().unwrap();
+    let part2: u32 = calorie_counts.iter().rev().take(3).sum();
+
+    (part1.to_string(), part2.to_string())
 }
